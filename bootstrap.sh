544e4a2679c45581daef934f27a4e8e3f6ad732f
Garat72 <32578454+Garat72@users.noreply.github.com>
2017-10-12 17:43:22 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,12 @@
-
+#!/bin/bash
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Instalace Raspberry Pi
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+COLOR_DEFAULT="\e[39m"
+COLOR_CYAN="\e[36m"
+COLOR_LIGHTBLUE="\e[1;34m"
+COLOR_YELLOW="\e[33m"
+COLOR_GREEN="\e[0;32m"
+COLOR_RED="\e[31m"
+COLOR_DARKRED="\e[1;31m"
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
