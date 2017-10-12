3571373eae986ba2d45b4dcec7a0f681d9266978
Garat72 <32578454+Garat72@users.noreply.github.com>
2017-10-12 18:12:25 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,3 +10,50 @@ COLOR_GREEN="\e[0;32m"
 COLOR_RED="\e[31m"
 COLOR_DARKRED="\e[1;31m"
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# FUNCTIONS
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+function hlavicka() {
+  clear
+  echo -e "${COLOR_LIGHTBLUE}"
+  echo -e ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
+  echo -e "                                                                                           "
+  echo -e " :::::::::  :::::::::   ::::::::  :::       ::::::::: :::    ::: :::::::::     ::::   :::: "
+  echo -e " :+:    :+: :+:    :+: :+:    :+: +:+       :+:       :+:    :+:    :+:         +:+: :+:+  "
+  echo -e " +:+    +:+ +:+    +:+ +:+    +:+ +:+       :+:       +:+    +:+    :+:           +:+:+    "
+  echo -e " +#+#+#+#:  +#+#+#+#:  +#+    +#+ +#+       +#+#+#    +#+#+#+#:     +#+    +#+     +#+     "
+  echo -e " +#+        +#+    +#+ +#+    +#+ +#+   +#+ +#+       +#+    +#+    +#+           #+#+#    "
+  echo -e " #+#        #+#    #+# #+#    #+# #+#   #+# #+#       #+#    #+#    #+#         +#+# #+#+  "
+  echo -e " ###        ###    ###  ########   #######  ######### ###    ###    ###        ####   #### "
+  echo -e "                                                                                           "
+  echo -e "###########################################################################################"
+  echo -e "################################### Test Raspberry Pi #####################################"
+  echo -e "###########################################################################################"
+  echo -e "${COLOR_DEFAULT}"
+}
+function display_info() {
+    echo -e ${COLOR_YELLOW}"${1}"${COLOR_DEFAULT}
+}
+function display_info_n() {
+    echo -e -n ${COLOR_YELLOW}"${1}"${COLOR_DEFAULT}
+}
+function display_title() {
+    echo -e ${COLOR_CYAN}"${1}"${COLOR_DEFAULT}
+}
+
+function display_error() {
+    echo -e ${COLOR_RED}"${1}"${COLOR_DEFAULT} 1>&2
+}
+function pokracovat(){
+  display_info_n "Pokračovat? (y/n) "
+  read prompt
+  if [ "$prompt" = "n" ]; then
+    exit
+  fi
+} 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+if [ "$(whoami)" != "root" ]; then
+  display_error "***Spusťte skript jako ROOT prosím. (sudo !!)***"
+  exit
+fi
